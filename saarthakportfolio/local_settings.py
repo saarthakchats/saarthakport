ef4c4ace4d4df2c920ebc0c3ea79eff70171da76
Saarthak Chaturvedi <saarthakchats05@gmail.com>
2019-07-08 16:57:13 +0000

    localsett

---
+++ saarthakportfolio/local_settings.py
@@ -0,0 +1,14 @@
+SECRET_KEY = 'm4ts1*4!##(l2sz!mi2mg%_zbx_g%51ab=csupariezg*sattusoupe41z5kwzkt('
+
+ALLOWED_HOSTS = ['139.59.56.231']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'saarthakport',
+        'USER': 'djangodbman',
+        'PASSWORD': 'Amitchats287',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
